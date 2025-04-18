ec05303d2377bf4358a03ea65e4f9149e1c3a867
mandojosue12 <mandojosue12@gmail.com>
2025-04-18 11:26:56 -0400

    Create style.css

---
+++ style.css
@@ -0,0 +1,48 @@
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  transition: background-color 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
+}
+
+#game {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+#monster {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.eye {
+  width: 20px;
+  height: 20px;
+  background: black;
+  border-radius: 50%;
+}
+
+#phone {
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: #ccc;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+}
